4a450511622053432003e3b468f594a1d913c03a
Isaac Mark <IzikM@users.noreply.github.com>
2023-06-10 10:43:50 +0300

    some cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { AwsHelp } from "./classes/AwsHelp";
 import { writeFileSync } from "fs";
 var x: AwsHelp = new AwsHelp(9999);
-writeFileSync('./theCommands.txt',JSON.stringify(x.theCommands,undefined,2));
+writeFileSync('./theCommands.json',JSON.stringify(x.theCommands,undefined,2));
 
